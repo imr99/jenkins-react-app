5b0fe2bec4f659e90c03ada347cc7eeb9cad2f87
bhaskarteja14529 <83528531+bhaskarteja14529@users.noreply.github.com>
2023-08-22 18:42:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Team, Thanks for given this Opportunity</h1>
-      <h2>This is a React-Js Application deployed by Mani Ratnam </h2>
+      <h2>This is a React-Js Application deployed by Mani Ratnam Inti</h2>
     </div>
   );
 }
